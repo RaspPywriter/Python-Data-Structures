c767ee019110e900a335273e75be043f8605ce23
RaspPywriter <39004382+RaspPywriter@users.noreply.github.com>
2019-02-25 20:07:59 -0500

    Update enum.py

--- enum.py
+++ enum.py
@@ -20,3 +20,14 @@ class Country(Enum):
 print('\nMember Name: {}'.format(Country.Albania.name))
 print('\nMember Value: {}'.format(Country.Albania.value))
 
+#print only the names in order by country value
+print('ordered by value: ')
+import enum
+class Country(enum.IntEnum):
+        Afghanistan = 93
+        Albania = 355
+        Algeria = 213
+        Andorra = 376
+        Angola = 244
+        Antarctica = 672
+print('\n'.join(' ' + s.name for s in sorted(Country)))
