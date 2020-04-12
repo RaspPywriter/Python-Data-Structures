50610cc3b5478cde74f288b0896acba3076443ae
RaspPywriter <39004382+RaspPywriter@users.noreply.github.com>
2020-04-12 13:55:21 -0400

    Practice program to calculate year you turn 100

---
+++ charInput.py
@@ -0,0 +1,20 @@
+from datetime import date
+def get_info():
+    name = input("Please enter your name ")
+    age = input("Please enter your age ")
+    return name, age
+
+def calc_age(x):
+    years = 100 - int(x)
+    curr_year = date.today().year
+    yr100 = int(curr_year) + int(years)
+    return yr100
+
+def print_message():
+    values = get_info()
+    #stores name and age in variable values
+    print("Hi " + values[0])
+    yr100 = calc_age(values[1])
+    print("In " + str(yr100) + " you will be 100!")
+
+print_message()
